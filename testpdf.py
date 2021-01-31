d07fef80b484da3f80df5475e19e3ed92b2544ac
beyrouti <michael@beyrouti.co>
2021-01-31 11:46:17 -0500

    First commit! Added readme and testpdf.py

---
+++ testpdf.py
@@ -0,0 +1,7 @@
+# #!/usr/bin/python3
+#
+# Author: Michael Beyrouti
+# Atlanta Fine Homes Sotheby's International Realty, IT Group
+#
+# This script is intended to parse the Network Setup form used for enboarding new members to AFH
+# Next iteration will send parsed data to Active Directory, creating the user with the data
