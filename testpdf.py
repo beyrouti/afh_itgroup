711d16d9f204b5812847cc54396bb3bcee5c4611
beyrouti <michael@beyrouti.co>
2021-01-31 14:29:21 -0500

    added .gitignore and member variables to testpdf.py

--- testpdf.py
+++ testpdf.py
@@ -5,3 +5,16 @@
 #
 # This script is intended to parse the Network Setup form used for enboarding new members to AFH
 # Next iteration will send parsed data to Active Directory, creating the user with the data
+
+import sys
+from pdfminer.pdfparser import PDFParser
+from pdfminer.pdfdocument import PDFDocument
+from pdfminer.pdftypes import resolve1
+
+member_name = ""
+member_license = ""
+member_address = ""
+member_email = ""
+member_cell = ""
+member_status = "" #Agent, Staff, Assistant, Broker, etc...
+member_copyCode = ""
