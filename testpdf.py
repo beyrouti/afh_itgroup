22474cc5e8c152ec2cf1d93c4dbf93acaf4173d9
beyrouti <michael@beyrouti.co>
2021-02-12 13:18:25 -0500

    prints the name and value of the text fields in the network form

--- testpdf.py
+++ testpdf.py
@@ -3,7 +3,7 @@
 # Author: Michael Beyrouti
 # Atlanta Fine Homes Sotheby's International Realty, IT Group
 #
-# This script is intended to parse the Network Setup form used for enboarding new members to AFH
+# This script is intended to parse the Network Setup form used for onboarding new members to AFH
 # Next iteration will send parsed data to Active Directory, creating the user with the data
 
 import sys
@@ -18,3 +18,15 @@ member_email = ""
 member_cell = ""
 member_status = "" #Agent, Staff, Assistant, Broker, etc...
 member_copyCode = ""
+
+# https://stackoverflow.com/questions/3984003/how-to-extract-pdf-fields-from-a-filled-out-form-in-python
+file_name = input("Enter Network Form file name: ")
+fp = open(file_name,'rb')
+parser = PDFParser(fp)
+network_form = PDFDocument(parser)
+fields = resolve1(network_form.catalog['AcroForm'])['Fields']
+for i in fields:
+    field = resolve1(i)
+    name, value = field.get('T'), field.get('V')
+    print('{0}: {1}'.format(name, value))
+
