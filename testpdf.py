d46c14ab5fb58d50b6631758bee8e03abd2016cd
beyrouti <michael@beyrouti.co>
2021-03-01 16:07:28 -0500

    user data from text fields is added to member_stats  dictionary

--- testpdf.py
+++ testpdf.py
@@ -1,4 +1,4 @@
-# #!/usr/bin/python3
+#!/usr/bin/python3
 #
 # Author: Michael Beyrouti
 # Atlanta Fine Homes Sotheby's International Realty, IT Group
@@ -18,9 +18,34 @@ member_email = ""
 member_cell = ""
 member_status = "" #Agent, Staff, Assistant, Broker, etc...
 member_copyCode = ""
+member_stats = {}
+#    "uEmail": "",
+#    "uName": "",
+#    "uCell": "",
+#    "uEmergencyContactNum": "",
+#    "uBirthday": "",
+#    "uStartDate": "",
+#    "uAfhEmail": "",
+#    "uNetworkLogin": "",
+#    "uCopyCode": "",
+#    "uCopyCode": "",
+#    "uDID": "",
+#    "uFaxNum": "",
+#    "uExtension": "",
+#    "uMobileDevice": "",
+#    "uTablet": "",
+#    "uComputer": "",
+#    "uTeam": "",
+#    "uBoard": "",
+#    "uFormerBrokerCode": "",
+#    "uRenewalDate": "",
+#    "uHomeAddress1": "",
+#    "uHomeAddress2": "",
+#    "uNightVMExt": "",
+#    "ulicenseNum": "",
 
 # https://stackoverflow.com/questions/3984003/how-to-extract-pdf-fields-from-a-filled-out-form-in-python
-file_name = input("Enter Network Form file name: ")
+file_name = "Network Setup Form - Mace Windu.pdf"
 fp = open(file_name,'rb')
 parser = PDFParser(fp)
 network_form = PDFDocument(parser)
@@ -28,5 +53,6 @@ fields = resolve1(network_form.catalog['AcroForm'])['Fields']
 for i in fields:
     field = resolve1(i)
     name, value = field.get('T'), field.get('V')
+    member_stats[str(name)] = str(value)
     print('{0}: {1}'.format(name, value))
-
+print(member_stats)
