4fff90228a321c68c57f75201aeee5ffd014a41f
beyrouti <michael@beyrouti.co>
2021-06-09 16:53:12 -0400

    added functionality for communication and editing of AD to ldap_test.py

--- ldap_test.py
+++ ldap_test.py
@@ -2,9 +2,12 @@
 
 from pyad import *
 
-pyad.set_defaults(ldap_server="afh.pri", username="asterisk", password="Shit$andwich747")
+pyad.set_defaults(ldap_server="10.0.254.3", username="asterisk", password="Shit$andwich747")
 
-user = aduser.ADUser.from_cn("jbravo", options=dict(ldap_server="10.0.254.3"))
-
-print(user)
+#user1 = aduser.ADUser.from_cn("Caroline Davenport")
+user1 = adobject.ADObject.from_dn("CN=Caroline Davenport,OU=BH Agents,OU=AFH Agents,DC=AFH,DC=pri")
+#OU=AFH Agents,OU=BH Agents,DC=AFH,DC=pri"
+print(user1)
+# user1.update_attribute("Company","anda")
+print(user1.get_attribute("Company"))
 
